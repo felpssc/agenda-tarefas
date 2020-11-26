2818d54afe4bfefd1b9829cb56cb8ee5ec9cf477
Fe1ipeSilva <carlosfelipesilva.fs@gmail.com>
2020-11-26 00:32:51 -0300

    oi

--- actions/connect_db.php
+++ actions/connect_db.php
@@ -1,9 +1,9 @@
 <?php 
 
-    $host        = "host=127.0.0.1";
+    $host        = "ec2-52-204-232-46.compute-1.amazonaws.com";
     $port        = "port=5432";
-    $dbname      = "dbname=projetotarefas";
-    $credentials = "user=postgres password=okdsmudar";
+    $dbname      = "dbname=d2tt54tc77jgfh";
+    $credentials = "user=qttkfeqqmbydet password=7aae424a6a712fbe8a0b82a3711fcd5c70d90fef6b1e0d849900730a058cbe7c";
 
     $connect = pg_connect("$host $port $dbname $credentials"  ) or die("Could not connect: " . pg_last_error());
 
